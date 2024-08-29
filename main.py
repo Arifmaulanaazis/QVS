dcb29ce8e81209ca0e6e514b463078ec5f40bc65
Arif Maulana Azis <titandigitalsoft@gmail.com>
2024-08-29 13:35:00 +0700

    add advance setting vina UI

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dfab7e434061cff065b5d0393021f899e242d15d4b0686018e06003c45f6b132
-size 130177450
+oid sha256:13bad41e0dfce8a8b4a241d9bf1ecabf37ef5d7814f38858c5234ec1e68f845f
+size 130181875
